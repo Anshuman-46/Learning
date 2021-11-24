27a3fe4878abe1723f9f5ec304bcf155537b8dce
Anshuman Shukla <88917068+Anshuman-46@users.noreply.github.com>
2021-11-25 00:21:10 +0530

    Add files via upload

---
+++ Mini Project/Guess_The_number.java
@@ -0,0 +1,36 @@
+package Mini_Project;
+import java.util.Scanner;
+public class Guess_The_number {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner (System.in);
+		
+		int myNumber=(int)(Math.random()*100);
+//		System.out.println(myNumber);
+		
+		int userNumber = 0;
+		
+		System.out.println("GUESS THE NUMBER :");
+		do
+		{
+			userNumber = sc.nextInt();
+			
+			if(myNumber == userNumber)
+			{
+				System.out.println("MOJ KARDI !! CORRECT NUMBER ");
+				break;
+			}
+			
+			else if(myNumber > userNumber)
+				System.out.println("YOUR NUMBER IS TOO SMALL");
+			
+			else 
+				System.out.println("YOUR NUMBER IS TOO LARGE");
+			
+		} while(userNumber >= 0);
+		
+		System.out.println("\nCORRECT NUMBER : "+myNumber);
+	}
+
+}
